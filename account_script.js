c4ebad6f2490ddbe6c5233bd73cde2202a3f141a
kohlmanndean <61201957+kohlmanndean@users.noreply.github.com>
2021-03-20 14:09:57 -0600

    render account page

--- account_script.js
+++ account_script.js
@@ -1,75 +1,57 @@
 $(document).ready(function () {
 	let currentUser;
-	init();
+
+	if (!signedIn) {
+		window.location.href = '/signin';
+	} else {
+		renderAccount();
+	}
 
 	$(document)
-		//show account update info
-		.on('click', '#accountUpdateButton', function () {
-			$('#accountUpdateButton').hide();
-			$('#existingAccountInfo').hide();
-			$('.account-info-update-form-wrap').show();
-			$('.account-menu-dropdown').hide();
-			$('#accountUpdateCancel').show();
+		//active tab indication
+		.on('click', '[data-tab]', function () {
+			$('.tab-indicator').removeClass('active');
+			$(this).find('.tab-indicator').addClass('active');
+			$('[data-tabpane]').hide();
+			$(`[data-tabpane="${$(this).attr('data-tab')}"]`).show();
 		})
-		//hide account update info
-		.on('click', '#accountUpdateCancel', function () {
-			$('#accountUpdateButton').show();
-			$('#existingAccountInfo').show();
-			$('.account-info-update-form-wrap').hide();
-			$('.account-menu-dropdown').show();
-			$('#accountUpdateCancel').hide();
+		//update or cancel update of email & pass
+		.on('click', '#updateProfile', function () {
+			$('[data-id="updateProfileItem"]').show();
+			$('[data-id="existingProfileItem"]').hide();
 		})
-		.on('click', '#accountUpdateConfirm', function () {
-			$('#existingAccountInfo').show();
-			$('.account-info-update-form-wrap').hide();
-			$('.account-menu-dropdown').show();
-			$('#accountUpdateCancel').hide();
+		.on('click', '#cancelUpdateProfile', function () {
+			$('[data-id="updateProfileItem"]').hide();
+			$('[data-id="existingProfileItem"]').show();
 		})
-		//show pause modal
-		.on('click', '#pauseModalOpen', function () {
-			$('#pauseModal').show();
+		//update or cancel update of shipping address
+		.on('click', '#updateShippingAddress', function () {
+			$('[data-id="updateAddressItem"]').show();
+			$('[data-id="existingAddressItem"]').hide();
 		})
-		//hide pause modal
-		.on('click', '#pauseModalClose', function () {
-			$('#pauseModal').hide();
+		.on('click', '#cancelUpdateAddress', function () {
+			$('[data-id="updateAddressItem"]').hide();
+			$('[data-id="existingAddressItem"]').show();
 		})
-		.on('click', '#pauseSubscriptionConfirm', function () {
-			$('#pauseModal').hide();
+		//subscription menu dropdown
+		.on('click', '#subscriptionMenuDropdownButton', function () {
+			$(this).closest('.subscription-menu-dropdown').find('.subscription-menu-dropdown-list').show();
 		})
-		//show cancel modal
-		.on('click', '#cancelModalOpen', function () {
-			$('#cancelModal').show();
-		})
-		.on('change', 'input[name="cancellation-reason"]', function () {
-			if ($(this).val() == 'Other') {
-				$('#otherReasonText').show();
-			} else {
-				$('#otherReasonText').hide();
+		.on('click', function (e) {
+			if (!$(e.target).hasClass('subscription-menu-dropdown-button')) {
+				$('.subscription-menu-dropdown-list').hide();
 			}
 		})
-		//hide cancel modal
-		.on('click', '#cancelModalClose', function () {
-			$('#cancelModal').hide();
-		})
-		.on('click', '#cancelSubscriptionConfirm', function () {
-			$('#cancelModal').hide();
-		})
-		//show update account modal
-		.on('click', '#updateAccountModalOpen', function () {
-			$('#updateAccountModal').show();
-		})
-		//hide update account modal
-		.on('click', '#updateAccountModalClose', function () {
-			$('#updateAccountModal').hide();
-		});
-	;
 
-	async function init() {
-		if (!signedIn) {
-			window.location.href = '/account/signin';
-		} else {
-			currentUser = await getCustomer(JSON.parse($.cookie('gumiAuth')).email, JSON.parse($.cookie('gumiAuth')).token);
-			$('[data-id="customerFirstName"]').html(await currentUser.first_name);
-		}
+		;
+
+	async function renderAccount() {
+		currentUser = await getCustomer(JSON.parse($.cookie('gumiAuth')).email, JSON.parse($.cookie('gumiAuth')).token);
+		$('[data-customer="firstName"]').html(currentUser.first_name);
+		$('[data-customer="email"]').html(currentUser.email);
+		$('#shippingAddress').html(`${currentUser.first_name} ${currentUser.last_name}<br>
+			${currentUser.street_address}<br>
+			${currentUser.city}, ${currentUser.state}, ${currentUser.zip}`);
+
 	}
 });
