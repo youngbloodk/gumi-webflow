183a8e00500b0148c2e1c5cf00daecfe61920ea8
kohlmanndean <61201957+kohlmanndean@users.noreply.github.com>
2021-06-03 21:06:32 -0600

    Formatting

--- search_results.js
+++ search_results.js
@@ -1,13 +1,12 @@
-$(document).ready(function() {
-	const search_term = getURLParam('query');
-	//google search tracking
-	gtag('event', 'search', {
-		search_term: search_term
-	});
+const search_term = getURLParam('query');
 
-	//klaviyo search event
-	var _learnq = _learnq || [];
-	_learnq.push(['track', 'item_search', {
-		search_term: search_term
-	}]);
+//google search tracking
+gtag('event', 'search', {
+	search_term: search_term
 });
+
+//klaviyo search event
+var _learnq = _learnq || [];
+_learnq.push(['track', 'Searched Site', {
+	search_term: search_term
+}]);
