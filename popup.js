da74cdf01686b2d4a5ed6ba44c76ecdf80f9fb02
kohlmanndean <61201957+kohlmanndean@users.noreply.github.com>
2021-06-23 15:39:15 -0600

    add popup js

---
+++ popup.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    if(!$.cookie('gumi_popup')) {
+        $('#popup-overlay').fadeIn();
+    }
+
+    $('[popup="close"]').click(function() {
+        $('#popup-overlay').fadeOut();
+        $.cookie('gumi_popup', moment().valueOf());
+    });
+
+    $('#email_signup').submit(function(e) {
+        const $email = $('#custEmail').val();
+
+        e.preventDefault();
+
+        if(!$email) {
+            alert('Please fill out all fields before submitting :)');
+        } else {
+            sendToKlaviyo($email).then(res => {
+                console.log(res);
+            });
+        }
+    });
+
+    async function sendToKlaviyo(email) {
+        const settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://manage.kmail-lists.com/ajax/subscriptions/subscribe",
+            "method": "POST",
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+                "cache-control": "no-cache"
+            },
+            "data": {
+                "g": "TdSCNw",
+                "$fields": "$source",
+                "email": email,
+                "$source": "Gumi Site"
+            }
+        };
+
+        $.ajax(settings).done(res => {
+            console.log(res);
+        });
+    }
+
+    //countdown timer
+    let now = moment().valueOf();
+    let today = moment().format('YYYY-MM-DD');
+    let midnight = moment(moment(today).add(1, 'day')).valueOf();
+    let duration = moment.duration(midnight - now);
+    let interval = 1000;
+
+    setInterval(function() {
+        duration = moment.duration(duration - interval, 'milliseconds');
+        $('#countdown').text(twoDigit(duration.hours()) + ":" + twoDigit(duration.minutes()) + ":" + twoDigit(duration.seconds()));
+    }, interval);
+
+    function twoDigit(n) {
+        return (n < 10 ? "0" : "") + n;
+    }
+});
