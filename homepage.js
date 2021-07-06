ca8db40a1efe36cfede06e1d5f264d5d926a4529
kohlmanndean <61201957+kohlmanndean@users.noreply.github.com>
2021-07-06 16:15:38 -0600

    add product filter by category

--- homepage.js
+++ homepage.js
@@ -18,4 +18,43 @@ $(document).ready(function() {
 		})
 		;
 	;
+
+	//filter handling
+	let filterActive = false;
+	$('[filter="button"]').click(function() {
+		const category = $(this).find($('input')).val();
+
+		if(filterActive && $(this).attr('class').indexOf('filter-active') >= 0) {
+			$('.filter-active').removeClass('filter-active');
+			filterActive = false;
+			$('[filter="item"]').fadeIn();
+		} else if(filterActive && $(this).attr('class').indexOf('filter-active') < 0) {
+			$('.filter-active').removeClass('filter-active');
+			$(this).addClass('filter-active');
+			filterActive = true;
+			filter(category);
+		} else {
+			$(this).addClass('filter-active');
+			filterActive = true;
+			filter(category);
+		}
+	});
+	function filter(category) {
+		const items = $('[filter="item"]');
+
+		items.each(function() {
+			const item = $(this);
+			const categories = [];
+
+			$(item).find($('[filter="item-category"]')).each(function() {
+				categories.push($(this).val());
+			});
+
+			if(categories.indexOf(category) >= 0) {
+				$(item).fadeIn();
+			} else {
+				$(item).fadeOut();
+			}
+		});
+	}
 });
