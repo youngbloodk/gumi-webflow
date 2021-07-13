24e97de6b6f36ffe1c9d63139bc0fd4db8d5f72e
kohlmanndean <61201957+kohlmanndean@users.noreply.github.com>
2021-07-13 11:33:27 -0600

    add sub items option

--- account/subscriptions/subscriptions_main.js
+++ account/subscriptions/subscriptions_main.js
@@ -7,8 +7,13 @@ $(document).ready(function() {
 	$(document)
 		//update subscription
 		.on('click', '[data-modalopen="Update-subscription"]', async function() {
+			let sub = $(this).closest('[data-stripeitem]');
+			let sub_id = sub.attr('data-stripeitem');
+			let sub_items = JSON.parse(decodeURI(sub.attr('data-subitems')));
+			let freq = parseInt(sub.attr('data-frequency'));
+			let renewal_date = sub.attr('data-periodend');
 			$('.loader-wrap').show();
-			renderSubItems(JSON.parse(decodeURI($(this).closest('[data-stripeitem]').attr('data-subitems'))), 'update')
+			renderSubItems(sub_items, 'update')
 				.then(items => {
 					$('#updateSubItemsList').empty().append(items.toString().replaceAll(',', ''));
 					const $items = $('#updateSubItemsList').find('[data-subitem]');
@@ -17,42 +22,52 @@ $(document).ready(function() {
 					}
 					$('.loader-wrap').fadeOut();
 				});
+			$('#updatedSubFrequency').val(freq);
+			$('#updatedSubRenewalDate').text(renewal_date);
+			await getSubitemOptions(sub_id)
+				.then(options => {
+					localStorage.setItem('gumiUpdateSubscriptionOptions', JSON.stringify(options.success));
+				});
 		})
 		.on('click', '#addSubItemsButton', function() {
-			let images = {};
-			renderItemAddOptions($(this).closest('[data-stripeitem]').attr('data-stripeitem')).then(options => {
-				$('#addSubItemsList').append(`
-					<div class="w-layout-grid grid _3col auto-auto-1fr">
-						<img src="" loading="lazy" width="60" sizes="(max-width: 479px) 17vw, 60px" alt="">
-						<div class="w-layout-grid grid _1col row-gap-5">
-							<div class= "select-wrapper small black">
-								<select class="field select" style="color:black; font-size:16px; height:24px; padding-left:2px;" data-id="add_item_name">
-									${options}
+			let options = JSON.parse(localStorage.getItem('gumiUpdateSubscriptionOptions'));
+			let optionsList = [];
+			for(const option of options) {
+				optionsList.push(`
+					<option value="${option.price_id}">${option.name}</option>
+				`);
+			}
+			$('#updateSubItemsList').append(`
+				<div data-id="add_item" style="padding-bottom:10px; margin-bottom:10px;">
+					<div class="w-layout-grid grid _2col _1fr-auto">
+						<div class="w-layout-grid grid _2col auto-1fr">
+							<div class="cell a-center j-center">
+								<a data-id="remove_sub_item" class="font-awesome _14"></a>
+							</div>
+							<div class="select-wrapper small black" style="height:40px; border-radius:8px;" >
+								<select class="field select white" name="item" style="padding-left:10px;">
+									${optionsList}
 								</select>
-								<div class="font-awesome _12" style="right:5px; color:black; position:absolute;"></div>
+								<div style="right:10px; position:absolute;" class="font-awesome _14"></div>
 							</div>
-							<div class="grid _2col column-gap-0 auto-1fr a-center">
-								<div class="text">Quantity:</div>
-								<div class= "select-wrapper small black" style = "width:40px; margin-left:5px;">
-									<select class="field select" style="color:black; font-size:16px; height:24px; padding-left:2px;" data-id="add_item_quant">
-										<option value="0">0</option>
-										<option value="1">1</option>
-										<option value="2">2</option>
-										<option value="3">3</option>
-										<option value="4">4</option>
-										<option value="5">5</option>
-										<option value="6">6</option>
-									</select>
-									<div class="font-awesome _12" style="right:5px; color:black; position:absolute;"></div>
-								</div>
-							</div>
-							<div class="text">Delivered: Every variable</div>
 						</div>
-						<div class="text right">$variable</div>
+						<div class="select-wrapper small black" style="height:40px; width:60px; border-radius:8px;">
+							<select class="field select white" name="quantity" style="padding-left:10px;">
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+								<option value="6">6</option>
+							</select>
+							<div style="right:10px; position:absolute;" class="font-awesome _14"></div>
+						</div>
 					</div>
-					<div class="divider"></div>`);
-			});
-
+				</div>
+			`);
+		})
+		.on('click', '[data-id="remove_sub_item"]', function() {
+			$(this).closest('[data-id="add_item"]').remove();
 		})
 		.on('click', '#updateSubscriptionConfirm', async function(e) {
 			e.preventDefault();
